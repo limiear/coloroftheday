00c5a5e0b9a2d143ccbda5e762a7eea6118d1f6b
eloy <eloy.colell@gmail.com>
2015-03-09 01:54:55 -0300

    It fixed the bug when upload the image.

--- coloroftheday/bot.py
+++ coloroftheday/bot.py
@@ -10,6 +10,7 @@ from colordescriptor import describe
 import time
 from twitter_keys import APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
 import random
+from StringIO import StringIO
 
 
 def twython(func):
@@ -38,15 +39,19 @@ class Presenter(object):
 
     def upload_media(self, image):
         with open(image, 'rb') as photo:
-            result = self.twitter.upload_media(media=photo)
+            result = StringIO(photo.read())
+            # result = self.twitter.upload_media(media=photo)['media_id']
         return result
 
     def tweet(self, status, images):
         time.sleep(10)
         template = random.choice(self.intros)
-        medias = map(lambda i: self.upload_media(i)['media_id'], images)
-        self.twitter.update_status(medias_id=medias,
-                                   status=template % status)
+        medias = map(lambda i: self.upload_media(i), images)
+        # self.twitter.update_status(medias_id=medias,
+        #                            status=template % status)
+        self.twitter.post('/statuses/update_with_media',
+                          params={'status': template % status,
+                                  'media': medias[0]})
         print template % status
 
     @twython
