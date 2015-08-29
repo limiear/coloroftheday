12de975dfb8dfbaeba17a9ef63bb09cc1764bbaf
root <root@rpi.(none)>
2015-08-29 14:21:43 -0300

    It try to follow more followers each time.

--- coloroftheday/followers_bot.py
+++ coloroftheday/followers_bot.py
@@ -293,7 +293,7 @@ def strategy():
     auto_unfollow_nonfollowers()
     better_group = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
     posible_group = len(huge_group) if len(huge_group) > 1000 else 1000
-    posible_group = (posible_group - len(better_group)) * 1.01
+    posible_group = (posible_group - len(better_group)) * 1.20
     print "Selecting trends..."
     countries = ['Worldwide', 'Argentina', 'Brazil', 'Chile', 'Spain']
     """
